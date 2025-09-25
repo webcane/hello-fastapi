7d2413d7b0fcb47a9bb28c1a336c7c2aa46ea4e7
Mikhail Niedre <mikhail.niedre@quantori.com>
2025-09-25 09:38:30 +0300

    rem lifespan

--- src/hello_app/core/security.py
+++ src/hello_app/core/security.py
@@ -1,8 +1,5 @@
-from fastapi import FastAPI
-from hello_app.core.config import settings
 from fastapi_azure_auth import SingleTenantAzureAuthorizationCodeBearer
-from contextlib import asynccontextmanager
-from typing import AsyncGenerator
+from hello_app.core.config import settings
 
 azure_scheme = SingleTenantAzureAuthorizationCodeBearer(
     tenant_id=settings.TENANT_ID,
@@ -11,12 +8,3 @@ azure_scheme = SingleTenantAzureAuthorizationCodeBearer(
         f'api://{settings.OPENAPI_CLIENT_ID}/user_impersonation': 'user_impersonation'
     },
 )
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    """
-    Load OpenID config on startup.
-    """
-    await azure_scheme.openid_config.load_config()
-    yield
--- src/hello_app/main.py
+++ src/hello_app/main.py
@@ -1,9 +1,8 @@
 from fastapi import Security, FastAPI
 from hello_app.core.config import settings
-from hello_app.core.security import azure_scheme, lifespan
+from hello_app.core.security import azure_scheme
 
 app = FastAPI(
-    lifespan=lifespan,
     title=settings.APP_NAME,
     openapi_url=f'{settings.API_PREF}/openapi.json',
     swagger_ui_oauth2_redirect_url='/oauth2-redirect',
